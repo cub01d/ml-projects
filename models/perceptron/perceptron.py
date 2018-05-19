a86d90c2322a2b8ea97d663984ed94027c0252a3
Kenneth Chan <kc135576@aol.com>
2018-05-19 01:03:41 -0700

    Fix indent

--- models/perceptron/perceptron.py
+++ models/perceptron/perceptron.py
@@ -17,9 +17,9 @@ class Perceptron:
         # fT dot a = t, where a is a point on line
         t = np.dot(self.f.T, a)[0]
         self.f = np.insert(self.f, 2, t, axis=0)
-        
+
         # now we have the target function!
-    
+
     def train(self, N):
         # generate training data
         X = np.random.uniform(-1, 1, (2, N))
@@ -29,14 +29,14 @@ class Perceptron:
 
         # label training data
         y = np.sign(np.dot(self.f.T, XX))[0]
-        
+
         # perceptron learning algorithm
         self.w = np.zeros([3,1])
         yhat = np.sign(np.dot(self.w.T, XX))
 
         # misclassified point indexes
         m = np.where(y != yhat)[1]
-        
+
         i = 0
         while True:
             i += 1
@@ -58,21 +58,21 @@ class Perceptron:
 
             # update misclassified points
             m = np.where(y != yhat)[1]
-        
+
         return i
-        
+
     def test(self, n=5000):
         # generate n test data points in test data set
         points = np.random.uniform(-1, 1, (2, n))
         points = np.insert(points, 2, 1, axis=0)
         labels = np.sign(np.dot(self.f.T, points))
-        
+
         yhat = np.sign(np.dot(self.w.T, points))
         error = np.where(yhat != labels)[0].size
-        
+
         return error/n
-    
-    numtrials = 1000
+
+numtrials = 1000
 N = 10
 iterations = 0
 errors = 0
@@ -82,6 +82,6 @@ for i in range(numtrials):
     p = Perceptron()
     iterations += p.train(N)
     errors += p.test()
-    
+
 print("avg # of iterations:", iterations/numtrials)
 print("avg probability:", errors/numtrials)
